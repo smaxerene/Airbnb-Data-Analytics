20724cecbf80b2b87b06b2e2b2a60740db5fd0c5
Samantha Quilat <270233431@yoobeestudent.ac.nz>
2024-05-23 23:08:13 +1200

    6th commit: individual listings & merging

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,50 +13,62 @@ def main():
 
     st.subheader("Individual Airbnb Data")
     
-    Jim = pd.read_csv("jim_airbnb_data.csv")
     st.write("1. Jim's Listing: 5 guests, 3 bedrooms, 4 beds, 2 baths")
+    Jim = pd.read_csv("jim_airbnb_data.csv")
     st.write(Jim)
     
-    Alex = pd.read_csv("alex_airbnb_data.csv")
     st.write("2. Alex's Listing: 6 guests, 2 bedrooms, 3 beds, 2 baths")
+    Alex = pd.read_csv("alex_airbnb_data.csv")
     st.write(Alex)
     
-    Glenda = pd.read_csv("glenda_airbnb_data.csv")
     st.write("3. Glenda's Listing: 5 guests, 3 bedrooms, 4 beds, 2 baths")
+    Glenda = pd.read_csv("glenda_airbnb_data.csv")
     st.write(Glenda)
     
-    Joshua = pd.read_csv("joshua_airbnb_data.csv")
+    
     st.write("4. Joshua's Listing: 4 guests, 2 bedrooms, 2 beds, 1 bath")
+    Joshua = pd.read_csv("joshua_airbnb_data.csv")
     st.write(Joshua)
     
-    Prince = pd.read_csv("prince_airbnb_data.csv")
     st.write("5. Prince's Listing: 4 guests, 2 bedrooms, 2 beds, 1 bath")
+    Prince = pd.read_csv("prince_airbnb_data.csv")
     st.write(Prince)
     
-    Tiffany = pd.read_csv("tiffany_airbnb_data.csv")
+    
     st.write("6. Tiffany's Listing: 4 guests, 2 bedrooms, 4 beds, 1 bath")
+    Tiffany = pd.read_csv("tiffany_airbnb_data.csv")
     st.write(Tiffany)
     
-    Michael = pd.read_csv("michael_airbnb_data.csv")
+    
     st.write("7. Michael's Listing: 6 guests, 2 bedrooms, 2 beds, 2.5 baths")
+    Michael = pd.read_csv("michael_airbnb_data.csv")
     st.write(Michael)
     
-    Noorani = pd.read_csv("noorani_airbnb_data.csv")
     st.write("8. Noorani's Listing: 3 guests, 2 bedrooms, 1 bath")
+    Noorani = pd.read_csv("noorani_airbnb_data.csv")
     st.write(Noorani)
     
-    Andy = pd.read_csv("andy_airbnb_data.csv")
+    
     st.write("9. Andy's Listing: 5 guests, 2 bedrooms, 3 beds, 1 bath")
+    Andy = pd.read_csv("andy_airbnb_data.csv")
     st.write(Andy)
     
-    Donghong = pd.read_csv("donghong_airbnb_data.csv")
     st.write("10. Donghong's Listing: 3 guests, 2 bedrooms, 5 beds, 1 bath")
+    Donghong = pd.read_csv("donghong_airbnb_data.csv")
     st.write(Donghong)
     
     st.subheader("Merged Airbnb Data")
-    AirbnbData = pd.concat([Jim, Alex, Glenda, Joshua, Prince, Tiffany, Michael, Noorani, Andy], ignore_index=True)
-    AirbnbData.drop(columns=["URL"], inplace=True)  # Drop the "URL" column
-    AirbnbData.fillna(0, inplace=True)  # Fill NaN values with 0
+    hosts = ["Jim", "Alex", "Glenda", "Joshua", "Prince", "Tiffany", "Michael", "Noorani", "Andy"]
+    
+    dfs = [pd.read_csv(f"{host.lower()}_airbnb_data.csv") for host in hosts]
+    for host, df in zip(hosts, dfs):
+        df["Host"] = host
+        
+    AirbnbData = pd.concat(dfs, ignore_index=True)
+    AirbnbData.drop(columns=["URL"], inplace=True)  # Drop the "URL" column and fill NaN values with 0
+    AirbnbData.fillna(0, inplace=True)
+   
+    st.subheader("Merged Airbnb Data")
     st.write(AirbnbData)
     
     st.subheader("Scatter Plots")
